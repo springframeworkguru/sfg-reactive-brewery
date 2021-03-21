6da239e915d00cd02aa7109c832b1de3d4e644fe
John Thompson <john@springframework.guru>
2021-03-21 12:00:00 -0400

    updating maven deps

--- target/generated-sources/annotations/guru/springframework/sfgrestbrewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestbrewery/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-03-21T11:26:25-0400",
+    date = "2021-03-21T11:53:02-0400",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.6 (Oracle Corporation)"
 )
 @Component
